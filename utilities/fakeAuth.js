54e7636547cc7959f42529a090eef30b7fe5063a
Seán Fitzgerald <seanfitzg@gmail.com>
2021-10-26 23:07:37 +0100

    rename plugin

--- utilities/fakeAuth.js
+++ utilities/fakeAuth.js
@@ -1,10 +1,10 @@
 import fp from 'fastify-plugin';
 
-async function myPlugin(app) {
+async function fakeAuth(app) {
   app.decorateRequest('user', null);
   app.addHook('onRequest', async (req, reply) => {
     req.user = { sub: '1234567890' };
   });
 }
 
-export default fp(myPlugin);
+export default fp(fakeAuth);
